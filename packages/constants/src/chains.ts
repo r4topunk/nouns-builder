8700ba4fb43fd48224d615f1e8c1bbef4e38ee96
dan13ram <dan13ram@gmail.com>
2026-06-03 15:21:16 +0530

    feat: show only testnet chains in testnet mode

--- packages/constants/src/chains.ts
+++ packages/constants/src/chains.ts
@@ -96,17 +96,17 @@ export const PUBLIC_IS_TESTNET = process.env.NEXT_PUBLIC_NETWORK_TYPE === 'testn
 // ----------------------
 
 // Still non-empty: concatenation of two non-empty arrays.
-const PUBLIC_ALL_CHAINS_UNSORTED = [
-  ...MAINNET_CHAINS_UNSORTED,
-  ...TESTNET_CHAINS_UNSORTED,
-] as const satisfies readonly Chain[]
+// const PUBLIC_ALL_CHAINS_UNSORTED = [
+//   ...MAINNET_CHAINS_UNSORTED,
+//   ...TESTNET_CHAINS_UNSORTED,
+// ] as const satisfies readonly Chain[]
 
-export const PUBLIC_ALL_CHAINS: Chains = sortNonEmptyChains(
-  PUBLIC_ALL_CHAINS_UNSORTED as NonEmptyArray<Chain>
-)
+// export const PUBLIC_ALL_CHAINS: Chains = sortNonEmptyChains(
+//   PUBLIC_ALL_CHAINS_UNSORTED as NonEmptyArray<Chain>
+// )
 
 const PUBLIC_DEFAULT_CHAINS_UNSORTED = PUBLIC_IS_TESTNET
-  ? PUBLIC_ALL_CHAINS_UNSORTED
+  ? TESTNET_CHAINS_UNSORTED
   : MAINNET_CHAINS_UNSORTED
 
 export const PUBLIC_DEFAULT_CHAINS: Chains = sortNonEmptyChains(
